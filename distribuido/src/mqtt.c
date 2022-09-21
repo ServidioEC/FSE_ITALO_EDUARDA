cba0fb55de61053ad895efdd563e07fac952dbcc
ServidioEC <52542729+ServidioEC@users.noreply.github.com>
2022-09-21 17:24:33 -0300

    Inclusão do token

--- distribuido/src/mqtt.c
+++ distribuido/src/mqtt.c
@@ -72,7 +72,8 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 void mqtt_start()
 {
     esp_mqtt_client_config_t mqtt_config = {
-        .uri = "mqtt://mqtt.eclipse.org",
+        .uri = "mqtt://164.41.98.25",
+        .username = "etiuuNa2iGUFT56IgPti"
     };
     client = esp_mqtt_client_init(&mqtt_config);
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
